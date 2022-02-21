5542cb02720b774819166be54c655bc6e083396f
Aarni Koskela <akx@iki.fi>
2022-02-21 21:31:25 +0200

    Become 0.2 (compatible with Django 3.0+)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='soctrack',
-    version='0.1',
+    version='0.2',
     description='Social Media Tracker',
     author='Aarni Koskela',
     author_email='akx@desucon.fi',
@@ -12,5 +12,5 @@ setup(
     packages=find_packages('.', include=('soctrack', 'soctrack.*')),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Django>=1.7', 'requests'],
+    install_requires=['Django>=3.0', 'requests', 'jsonfield'],
 )
--- soctrack/urls.py
+++ soctrack/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from soctrack.views import RecentSocialView
 
 urlpatterns = [
-    url(r'^api/social/$', cache_page(15)(RecentSocialView.as_view())),
+    path('api/social/', cache_page(15)(RecentSocialView.as_view())),
 ]
